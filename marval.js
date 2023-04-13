181a3e586a763923efff699ac3f2d17801ef30a5
kalaiarasanRinish <130643969+kalaiarasanRinish@users.noreply.github.com>
2023-04-13 13:21:15 +0530

    Add files via upload

---
+++ marval.js
@@ -0,0 +1,34 @@
+// const button=document.getElementById("tab1")
+// button.addEventListener('click',()=>{
+//      window.open("https://www.google.com/search?q=marvel+news&rlz=1C1YTUH_enIN1023IN1023&oq=marval+&aqs=chrome.5.69i59l4j69i57j0i10i67j0i10i131i433i512j69i60.5901j0j7&sourceid=chrome&ie=UTF-8")
+
+// })  
+
+
+// const button1=document.getElementById("tab2")
+// button1.addEventListener('click',()=>{
+//     window.open("https://www.google.com/search?gs_ssp=eJzj4tDP1TcwtcgyUmA0YHRg8OLNTSwqS81RSM7PzUwuBgBpQwgl&q=marvel+comics&rlz=1C1YTUH_enIN1023IN1023&oq=marval+comi&aqs=chrome.1.69i57j46i10i131i199i433i465i512j0i10i131i433i512j0i10i512l2j0i10i433i512j0i10i512l3.6969j0j7&sourceid=chrome&ie=UTF-8")
+// })
+
+
+// const button2=document.getElementById("tab3")
+//     button2.addEventListener('click',()=>{
+//   window.open("https://www.google.com/search?q=marvel+characters&rlz=1C1YTUH_enIN1023IN1023&oq=marval+ch&aqs=chrome.1.69i57j0i10i433i512l2j0i10i131i433i512j0i10i512l5j46i10i340i512.3414j0j7&sourceid=chrome&ie=UTF-8")
+//     })
+
+
+//     const button3=document.getElementById("tab4")
+//     button3.addEventListener('click',()=>{
+//         window.open("https://www.google.com/search?q=marval+movies&rlz=1C1YTUH_enIN1023IN1023&oq=marval+movies&aqs=chrome..69i57j0i10i131i433i512j69i59j0i10i512j0i10i131i433i512j0i10i433i512j0i10i512j0i10i131i433i512j0i10i512l2.5006j0j7&sourceid=chrome&ie=UTF-8")
+//     })
+
+
+//     const button4=document.getElementById("tab5")
+//     button4.addEventListener('click',()=>{
+//         window.open("https://www.google.com/search?q=marvel+tv+series&rlz=1C1YTUH_enIN1023IN1023&oq=marval+tv+&aqs=chrome.1.69i57j0i10i433i512j0i10i512l8.5904j0j7&sourceid=chrome&ie=UTF-8")
+//     })
+
+//     const button5=document.getElementById("tab6")
+//     button5.addEventListener('click',()=>{
+//         window.open("https://www.google.com/search?q=marvel+video&rlz=1C1YTUH_enIN1023IN1023&oq=marval+vide&aqs=chrome.1.69i57j0i10i512l9.10610j0j7&sourceid=chrome&ie=UTF-8")
+//     })
